b7d6266055ebbdb8b05309d79091ca2437fe7310
Amanur Rahman <59202695+Araman99@users.noreply.github.com>
2021-11-01 15:05:48 +0600

    ar-bro

--- lbr1/lbr1_4.sh
+++ lbr1/lbr1_4.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-read -p "Enter the number: " num
-len=${#num}
-largest=${num[0]}
+# read -p "Enter the number: " num
+# len=${#num}
+# largest=0
 
-for ((i=0; i<$len; i++))
+# for ((i=0; i<$len; i++))
+# do
+# if [[ "$largest" < "${num:i:0}" ]]
+# then
+#  echo 'hi'
+# fi
+#  echo "hello `${num[$i]}`"
+# done
+
+# echo $largest
+# echo $num
+
+# array=(1 2 3 4 5` 6 7)
+
+read -p "Enter the number: " array
+
+min=0
+# max=$(( ${#array[@]} -1 ))
+max=${#array}
+echo $max
+while [[ min -lt max ]]
 do
-if (( $largest < ${num:i:0} )); then
- echo 'hi'
-fi
- echo 'hello'
+    # Swap current first and last elements
+    x="$array[$min]"
+    echo "Hi $x"
+    array[$min]="${array[$max]}"
+    array[$max]="$x"
+    # Move closer
+    (( min++, max-- ))
 done
 
-echo $largest
-echo $num
+echo "${array[@]}"
